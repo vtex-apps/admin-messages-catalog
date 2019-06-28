ef221c64077b8e162c36f5448a792c3462f9c367
Felippe Nardi <felippe@nardi.me>
2019-06-28 13:19:46 -0300

    Translate first step screen

--- react/AdminMessages.tsx
+++ react/AdminMessages.tsx
@@ -1,68 +1,133 @@
 import React, {FC} from 'react'
+import {defineMessages, InjectedIntl, injectIntl} from 'react-intl'
 import {Box, Button, Layout, PageBlock, PageHeader} from 'vtex.styleguide'
 
-const AdminMessages: FC = () => {
+interface Props {
+  intl: InjectedIntl
+}
+
+const AdminMessages: FC<Props> = ({intl}) => {
   return (
     <div className="min-vh-100 bg-muted-5">
-      <Layout pageHeader={<PageHeader title="Products translation" />}>
+      <Layout
+        pageHeader={
+          <PageHeader
+            title={intl.formatMessage({id: 'admin/messages.headerTitle'})}
+          />
+        }>
         <PageBlock>
           <div className="bb b--muted-4  nh7 ph7 pb7 mb7">
             <div className="flex">
               <div>
                 <p className="mb1 mt0">
-                  1. Export your product catalog to email
+                  {intl.formatMessage({
+                    id: 'admin/messages.step1.instruction1',
+                  })}
                 </p>
                 <p className="t-mini c-muted-2 mt1 pl5">nardi@vtex.com.br</p>
               </div>
               <div className="flex-grow-1 tr">
-                <Button variation="secondary">Export Catalog</Button>
+                <Button variation="secondary">
+                  {intl.formatMessage({
+                    id: 'admin/messages.step1.exportCatalog',
+                  })}
+                </Button>
               </div>
             </div>
           </div>
           <p className="mv7">
-            2. Depois de exportar, abra o seu e-mail e faça o download do
-            arquivo em anexo
+            {intl.formatMessage({
+              id: 'admin/messages.step1.instruction2',
+            })}
           </p>
           <p className="mv7">
-            3. Abra o arquivo <b>catalog.csv</b> no Excel ou no Numbers
+            {intl.formatMessage(
+              {
+                id: 'admin/messages.step1.instruction3',
+              },
+              {file: 'catalog.csv'}
+            )}
           </p>
           <div>
             <p className="mv7">
-              4. Traduza, na planilha, o conteúdo das seguintes colunas:
+              {intl.formatMessage({
+                id: 'admin/messages.step1.instruction4',
+              })}
             </p>
             <Box noPadding>
               <div className="c-muted-1 ph5 pv2">
                 <p>
-                  <span className="mr3 fw5">_ProductName (Required)</span>{' '}
-                  <span className="f6">Nome</span>
+                  <span className="mr3 fw5">
+                    _ProductName (
+                    {intl.formatMessage({
+                      id: 'admin/messages.required',
+                    })}
+                    )
+                  </span>{' '}
+                  <span className="f6">
+                    {intl.formatMessage({
+                      id: 'admin/messages.step1.name',
+                    })}
+                  </span>
                 </p>
                 <p>
                   <span className="mr3 fw5">_Keywords</span>{' '}
-                  <span className="f6">Palavras substitutas</span>
+                  <span className="f6">
+                    {intl.formatMessage({
+                      id: 'admin/messages.step1.keywords',
+                    })}
+                  </span>
                 </p>
                 <p>
                   <span className="mr3 fw5">_SiteTitle</span>{' '}
-                  <span className="f6">Título da página</span>
+                  <span className="f6">
+                    {intl.formatMessage({
+                      id: 'admin/messages.step1.siteTitle',
+                    })}
+                  </span>
                 </p>
                 <p>
                   <span className="mr3 fw5">_ProductDescription</span>{' '}
-                  <span className="f6">Descrição do produto</span>
+                  <span className="f6">
+                    {intl.formatMessage({
+                      id: 'admin/messages.step1.productDescription',
+                    })}
+                  </span>
                 </p>
                 <p>
                   <span className="mr3 fw5">_MetaTagDescription</span>{' '}
-                  <span className="f6">Descrição (Meta tag Description)</span>
+                  <span className="f6">
+                    {intl.formatMessage({
+                      id: 'admin/messages.step1.metaTagDescription',
+                    })}
+                  </span>
                 </p>
               </div>
             </Box>
           </div>
           <p className="mv7">
-            5. Após traduzir, salve o arquivo e clique em <b>Next</b> para
-            continuar
+            {intl.formatMessage({
+              id: 'admin/messages.step1.instruction5',
+            })}
           </p>
           <div className="flex items-center">
-            <div className="c-muted-1 f6">Step 1 of 2</div>
+            <div className="c-muted-1 f6">
+              {intl.formatMessage(
+                {
+                  id: 'admin/messages.stepCounter',
+                },
+                {
+                  current: 1,
+                  total: 2,
+                }
+              )}
+            </div>
             <div className="flex-grow-1 tr">
-              <Button>Next</Button>
+              <Button>
+                {intl.formatMessage({
+                  id: 'admin/messages.next',
+                })}
+              </Button>
             </div>
           </div>
         </PageBlock>
@@ -71,4 +136,4 @@ const AdminMessages: FC = () => {
   )
 }
 
-export default AdminMessages
+export default injectIntl(AdminMessages)
